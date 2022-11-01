e621724f9b788e5b2c14dad0ef6564ebcf21f58b
galihprad <galih.p.w.j@gmail.com>
2022-11-01 15:14:02 +0700

    Refinement 3

---
+++ helper/getMonthName.js
@@ -0,0 +1,17 @@
+export function getMonthName(monthNum) {
+  const monthList = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  return monthList[monthNum - 1];
+}
